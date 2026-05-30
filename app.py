080906150e55aa74f71ca35c6706303b7730db6a
bmp786 <bhannumalliprasad@gmail.com>
2026-05-30 11:59:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template
+from routes.authRoutes import auth_bp
+from routes.productRoutes import product_bp
+from routes.paymentRoutes import payment_bp
+from routes.orderRoutes import order_bp
+
+app = Flask(__name__)
+
+app.register_blueprint(auth_bp)
+app.register_blueprint(product_bp)
+app.register_blueprint(payment_bp)
+app.register_blueprint(order_bp)
+
+
+@app.route('/')
+def home():
+    return {
+        "message": "SkillKart API Running"
+    }
+
+
+@app.route('/payment-page')
+def payment_page():
+    return render_template("payment.html")
+
+@app.route('/map')
+def map_page():
+    return render_template("map.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
